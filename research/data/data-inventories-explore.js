db572fc3b177a31031c45b76f54be983bac4be36
Alan Palazzolo <apalazzolo@minnpost.com>
2015-03-02 14:44:22 -0500

    Using list of valid data.json to compile some analysis.

--- research/data/data-inventories-explore.js
+++ research/data/data-inventories-explore.js
@@ -4,86 +4,138 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 var _ = require('lodash');
+var queue = require('queue-async');
 
-// Full data
+// Some variables
 var data = [];
+var dlQueue = queue(4);
+var ids;
 
 // List of inventories
-var urls = require(path.join(__dirname, 'data-inventories.json'));
+var urls = require(path.join(__dirname, '../scraper/with-data.json'));
 
-// Local files
-var locals = _.map(urls, function(u, ui) {
-  var l = {
-    path: path.join(__dirname, 'original', ui + '.json')
-  };
-  return l;
+// Determine local files
+urls = _.map(urls, function(u, ui) {
+  u.id = u['Domain Name'].toLowerCase().replace(/\s/ig, '-');
+  u.path = path.join(__dirname, 'original', u.id + '.json')
+  return u;
 });
 
-// TODO download.  Just did manually with:
+// Download (if needed)
+console.log('Downloading files if needed ... ');
+_.each(urls, function(u, ui) {
+  if (!fs.existsSync(u.path)) {
 
-/*
-curl -L http://epa.gov/data.json > research/data/original/epa.json && \
-curl -L http://opm.gov/data.json > research/data/original/opm.json && \
-curl -L http://va.gov/data.json > research/data/original/va.json && \
-curl -L http://dol.gov/data.json > research/data/original/dol.json
-*/
+    // Queue up request
+    dlQueue.defer(function(done) {
+      console.log('Downloading ' + u['Data URL'] + ' ... ');
 
-// Load files
-locals = _.map(locals, function(l, li) {
-  l.data = require(l.path);
-
-  // Concat data
-  data = data.concat(l.data.dataset);
-  return l;
+      request.get(u['Data URL'], function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          fs.writeFileSync(u.path, body);
+          done();
+        }
+        else {
+          console.error('Error getting JSON file.', error, u);
+        }
+      });
+    });
+  }
 });
 
+// When done
+dlQueue.awaitAll(function(error, results) {
+  if (error) {
+    console.error('Error with queue.', error);
+  }
+  console.log('Concatenating files and analyzing ... ');
 
-// Some stats
-// Description of schema: https://project-open-data.cio.gov/schema/
+  // Load files
+  urls = _.map(urls, function(u, ui) {
+    u.data = require(u.path);
+    return u;
+  });
 
-// Count
-console.log('Dataset count: ', data.length);
-console.log('============= \n');
+  // Concatenate datasets for easier stats
+  _.each(urls, function(u, ui) {
+    data = data.concat(u.data.dataset);
+  });
 
-// Types of access level
-console.log('Access count: ', _.map(_.groupBy(data, 'accessLevel'), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // Make sure there are no empty rows
+  data = _.filter(data, function(d, di) {
+    return _.isObject(d);
+  });
 
-// Type
-console.log('Type count: ', _.map(_.groupBy(data, '@type'), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // There are some duplicates.  We use the publisher name and identifier to
+  // determine if the same dataset.  It looks like the identifier is unique
+  // but it doesn't technically need to be unique across agencies
+  ids = {};
+  _.each(data, function(d, di) {
+    ids[d.identifier + d.publisher.name] = d;
+  });
+  data = _.values(ids);
 
-// Rights is usually not there, but also unique
+  // Some stats
+  // Description of schema: https://project-open-data.cio.gov/schema/
 
-// Orgs can be departments and in some cases contracters
+  // Count
+  console.log('Dataset count: ', data.length);
+  console.log('============= \n');
 
-// Has URL
-console.log('Has URL count: ', _.map(_.groupBy(data, function(d, di) {
-  return (d.accessURL) ? 'Has URL' : 'No URL';
-}), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // Types of access level
+  console.log('Access count: ', _.map(_.groupBy(data, 'accessLevel'), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
 
-// License
-console.log('License count: ', _.map(_.groupBy(data, 'license'), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // Type
+  console.log('Type count: ', _.map(_.groupBy(data, '@type'), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
 
-// Period
-// https://project-open-data.cio.gov/iso8601_guidance/#accrualperiodicity
-console.log('Periodicity count: ', _.map(_.groupBy(data, 'accrualPeriodicity'), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // Rights is usually not there, but also unique
 
-// Data quality (boolean)
-console.log('Data quailty count: ', _.map(_.groupBy(data, 'dataQuality'), function(d, di) {
-  return di + ': '  + d.length;
-}));
-console.log('============= \n');
+  // Orgs can be departments and in some cases contracters
+
+  // Has URL
+  console.log('Has URL count: ', _.map(_.groupBy(data, function(d, di) {
+    return (d.accessURL) ? 'Has URL' : 'No URL';
+  }), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
+
+  // License
+  /*
+  console.log('License count: ', _.map(_.groupBy(data, 'license'), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
+  */
+
+  // Period
+  // https://project-open-data.cio.gov/iso8601_guidance/#accrualperiodicity
+  console.log('Periodicity count: ', _.map(_.groupBy(data, 'accrualPeriodicity'), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
+
+  // Data quality (boolean)
+  console.log('Data quailty count: ', _.map(_.groupBy(data, 'dataQuality'), function(d, di) {
+    return di + ': '  + d.length;
+  }));
+  console.log('============= \n');
+
+  // Identifier count
+  console.log('Identifier >2 name count: ')
+  _.each(_.groupBy(data, function(d, di) {
+    return d.identifier;
+  }), function(d, di) {
+    if (d.length > 1) {
+      console.log(di + ': '  + d.length);
+    }
+  });
+  console.log('============= \n');
+
+});
